04be5ff7819915c1779ae5a869e81883a3412b0a
Dave Shepard <kynatro@gmail.com>
2026-03-16 23:42:39 -0700

    Execute pr creation in postversion script

--- scripts/postversion.js
+++ scripts/postversion.js
@@ -6,5 +6,13 @@ const branch = `release/v${version}`;
 execSync(`git push -u origin ${branch}`);
 console.log(`Pushed branch ${branch}`);
 
+execSync(
+  `gh pr create --base main --head "${branch}" --title "Release: v${version}" --fill-verbose`,
+  { stdio: 'inherit' }
+);
+console.log(`Created pull request for ${branch}`);
+
 execSync(`git tag -d v${version}`);
 console.log(`Deleted local tag v${version}`);
+
+execSync(`git checkout dev`);
