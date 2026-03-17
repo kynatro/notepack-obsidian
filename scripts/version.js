6e3f32af97f7bc9edaa85a15911397ab64340766
Dave Shepard <kynatro@gmail.com>
2026-03-16 22:37:12 -0700

    Add preversion and version scripts

---
+++ scripts/version.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const manifestPath = path.join(__dirname, '../src/manifest.json');
+const { version } = require('../package.json');
+
+const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
+manifest.version = version;
+
+fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2) + '\n');
+console.log(`Updated manifest.json to version ${version}`);
