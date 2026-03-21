cd725f818c5365f773c5cfad94031c97ccdeda2c
kynatro <kynatro@gmail.com>
2026-03-21 07:35:38 -0700

    Create release branch without switching and push dev and release branch

--- scripts/postversion.js
+++ scripts/postversion.js
@@ -3,8 +3,11 @@ const { execSync } = require('child_process');
 const { version } = require('../package.json');
 const branch = `release/v${version}`;
 
-execSync(`git push -u origin ${branch}`);
-console.log(`Pushed branch ${branch}`);
+execSync(`git branch ${branch}`);
+console.log(`Created branch ${branch}`);
+
+execSync(`git push -u origin dev ${branch}`);
+console.log(`Pushed branches dev, ${branch}`);
 
 execSync(
   `gh pr create --base main --head "${branch}" --title "Release: v${version}" --fill-verbose`,
