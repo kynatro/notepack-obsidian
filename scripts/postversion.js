1373fe6e0ee01bfa41c070be5b958fc0cbca4415
Dave Shepard <kynatro@gmail.com>
2026-03-21 11:26:14 -0700

    MAINT: Local release flow (#21)
    * Modify postversion to merge to main and push instead of creating a PR
    
    * Remove gh checks from preversion
    
    * Fix typos

--- scripts/postversion.js
+++ scripts/postversion.js
@@ -2,7 +2,6 @@ const { execSync } = require('child_process');
 const readline = require('readline');
 
 const { version: RELEASE_VERSION } = require('../package.json');
-const RELEASE_BRANCH = `release/v${RELEASE_VERSION}`;
 
 function confirm(question) {
   const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
@@ -27,7 +26,7 @@ function rollback() {
 }
 
 async function run() {
-  const confirmed = await confirm(`Push branches dev and ${RELEASE_BRANCH} and create a PR? (y/N) `);
+  const confirmed = await confirm(`Merge dev@${RELEASE_VERSION} to main and push to origin? (y/N) `);
 
   if (!confirmed) {
     console.log('Push cancelled, cleaning up...');
@@ -35,17 +34,13 @@ async function run() {
     return;
   }
 
-  execSync(`git branch ${RELEASE_BRANCH}`);
-  console.log(`Created release branch ${RELEASE_BRANCH}`);
-
-  execSync(`git push -u origin dev ${RELEASE_BRANCH}`);
-  console.log(`Pushed branches dev, ${RELEASE_BRANCH}`);
-
-  execSync(
-    `gh pr create --base main --head "${RELEASE_BRANCH}" --title "Release: v${RELEASE_VERSION}" --fill-verbose`,
-    { stdio: 'inherit' }
-  );
-  console.log(`Created pull request for ${RELEASE_BRANCH}`);
+  execSync(`git fetch origin main`);
+  execSync(`git checkout main`);
+  execSync(`git reset --hard origin/main`);
+  execSync(`git merge dev`);
+  execSync(`git push -u origin dev main`);
+  console.log(`Pushed branches dev, main`);
+  execSync(`git checkout dev`);
 
   cleanupTag();
 }
