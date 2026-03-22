781b1946f8b09de166932072a7db2ae216e3a3b2
kynatro <kynatro@gmail.com>
2026-03-21 23:24:35 -0700

    Rename tag to match Obsidian expected schema and cleanup postversion script messaging

--- scripts/postversion.js
+++ scripts/postversion.js
@@ -29,15 +29,19 @@ async function run() {
     return;
   }
 
-  execSync(`git fetch origin main`);
-  execSync(`git checkout main`);
-  execSync(`git reset --hard origin/main`);
-  execSync(`git merge dev`);
-  execSync(`git push -u origin dev main`);
-  console.log(`Pushed branches dev, main`);
-  execSync(`git push --tags origin`);
-  console.log(`Pushed tags`);
-  execSync(`git checkout dev`);
+  execSync('git fetch origin main');
+  execSync('git checkout main');
+  execSync('git reset --hard origin/main');
+  execSync('git merge dev');
+  console.log('Merged dev into main');
+  execSync('git push -u origin dev main');
+  console.log('Pushed branches dev, main');
+  execSync(`git tag ${RELEASE_VERSION} v${RELEASE_VERSION}`);
+  execSync(`git tag -d v${RELEASE_VERSION}`);
+  console.log(`Rename tag v${RELEASE_VERSION} to ${RELEASE_VERSION}`);
+  execSync('git push --tags origin');
+  console.log('Pushed tags to origin');
+  execSync('git checkout dev');
 }
 
 run();
